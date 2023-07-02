a4658dd948de8d1deaec5a3d99da3ee5d4745fba
Lily Skye <me@suchipi.com>
2023-07-02 14:27:26 -0600

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env yavascript
 import * as std from "quickjs:std";
 import * as os from "quickjs:os";
-import { parseInputString } from "./parse-input-string";
+import { ArgPart, parseInputString } from "./parse-input-string";
 import { stringifyArgParts } from "./stringify-arg-parts";
 
 // TODO put in yavascript
@@ -27,17 +27,30 @@ function which(binaryName: string): Path | null {
   return null;
 }
 
-// TODO use startRepl with custom line handler instead
-while (true) {
-  std.out.puts("$ ");
-  const line = std.in.getline()?.trim();
-  if (line == null) break;
+const RESET = reset("");
+
+globalThis.prompt = function () {
+  const username = $("whoami").stdout.trim();
+  const hostname = $("hostname").stdout.trim();
+
+  let dir = pwd();
+  if (env.HOME) {
+    dir = dir.replace(new RegExp("^" + RegExp.escape(env.HOME)), "~");
+  }
+
+  return `${green(username)}@${hostname} ${cyan(dir)}${RESET} \$ `;
+};
+
+function handleInput(input: string) {
+  if (input.trim() == "") {
+    return;
+  }
 
   try {
-    const parts = line.split(/\s|\b/);
+    const parts = input.split(/\s|\b/);
     const firstPart = parts[0];
     if (firstPart == null || firstPart === "") {
-      continue;
+      return;
     }
 
     let program: null | Path | ((...args: any) => any) = null;
@@ -56,14 +69,14 @@ while (true) {
 
     if (program == null) {
       targetName = "eval js";
-      console.log(dim("-> eval js: " + line));
-      result = std.evalScript(line, {
+      console.log(dim("-> eval js: " + input));
+      result = std.evalScript(input, {
         backtraceBarrier: true,
         filename: "<command-line>",
       });
     } else if (program instanceof Path) {
       targetName = "exec program";
-      const execArgs = parseInputString(line)
+      const execArgs = parseInputString(input)
         .filter((part) => part.type !== "gap")
         .map((part) => (part as any).content);
 
@@ -78,19 +91,19 @@ while (true) {
       } else {
         if (secondPart.startsWith("(")) {
           targetName = "eval js";
-          console.log(dim("-> eval js: " + line));
-          result = std.evalScript(line, {
+          console.log(dim("-> eval js: " + input));
+          result = std.evalScript(input, {
             backtraceBarrier: true,
             filename: "<command-line>",
           });
-        } else if (line === firstPart + ".") {
+        } else if (input === firstPart + ".") {
           targetName = "print var";
           console.log(dim("-> print var: " + firstPart));
           result = program;
         } else {
           targetName = "eval js w/ args";
 
-          const parts = parseInputString(line);
+          const parts = parseInputString(input);
           const partsAfterFirst = parts.slice(1, parts.length);
 
           // remove leading gap if present
@@ -98,25 +111,22 @@ while (true) {
             partsAfterFirst.shift();
           }
 
-          const partsWithHomeExpanded = partsAfterFirst.map((part) => {
-            if (
-              part.type === "gap" ||
-              part.type === "single-quoted" ||
-              part.type === "backticks"
-            ) {
-              return part;
-            }
-
-            const HOME = env.HOME;
-            if (!HOME) {
-              return part;
-            }
-
-            return {
-              type: part.type,
-              content: part.content.replace(/~/g, HOME),
-            };
-          });
+          const HOME = env.HOME;
+          let partsWithHomeExpanded: Array<ArgPart>;
+          if (HOME) {
+            partsWithHomeExpanded = partsAfterFirst.map((part) => {
+              if (part.type === "bare" || part.type === "double-quoted") {
+                return {
+                  type: part.type,
+                  content: part.content.replace(/~/g, HOME),
+                };
+              } else {
+                return part;
+              }
+            });
+          } else {
+            partsWithHomeExpanded = partsAfterFirst;
+          }
 
           const args = stringifyArgParts(partsWithHomeExpanded, {
             quoteBare: true,
@@ -146,3 +156,12 @@ while (true) {
     console.error(err);
   }
 }
+
+const interactivePrompt = new InteractivePrompt(handleInput, {
+  historyFileName: "yosh_repl_history.txt",
+  prompt: () => {
+    return (globalThis.prompt as any)();
+  },
+});
+
+interactivePrompt.start();
