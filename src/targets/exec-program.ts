208c0649cdc1a1ad3a9fd3c9f09d5d22cd04b00c
Lily Skye <me@suchipi.com>
2024-11-04 19:14:07 -0700

    use stringifyArgParts in execProgram

--- src/targets/exec-program.ts
+++ src/targets/exec-program.ts
@@ -1,9 +1,15 @@
 import { parseInputString } from "../parse-input-string";
+import { stringifyArgParts } from "../stringify-arg-parts";
 
 export function execProgram(input: string) {
-  const execArgs = parseInputString(input)
-    .filter((part) => part.type !== "gap")
-    .map((part) => (part as any).content);
+  const argParts = parseInputString(input);
+  const execArgs = stringifyArgParts(
+    argParts.filter((part) => part.type !== "gap"),
+    {
+      quoteBare: false,
+      gapReplacement: "",
+    }
+  );
 
   console.log(
     dim(`-> exec(${JSON.stringify(execArgs)}, { failOnNonZeroStatus: false });`)
